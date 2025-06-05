44c5b0e5b8ed33a4c91f5112e7025c7f682ca04a
jojo051 <youpi051@yahoo.fr>
2025-06-05 12:41:49 +0200

    add git hub in skill

--- src/components/Other.tsx
+++ src/components/Other.tsx
@@ -1,12 +1,18 @@
 import biomeSymbol from "../assets/biome-symbol.png";
+import githubSymbol from "../assets/github-symbol-white.png";
+
 const Other = () => {
 	return (
 		<div>
 			<h3 className="font-semibold text-2xl text-center">Other</h3>
 			<ul>
 				<li className="flex items-baseline justify-center">
-					<img className="size-4" src={biomeSymbol} alt="Logo figma" />
-					Figma
+					<img className="size-4" src={githubSymbol} alt="Logo github" />
+					Github
+				</li>
+				<li className="flex items-baseline justify-center">
+					<img className="size-4" src={biomeSymbol} alt="Logo biome" />
+					Biome
 				</li>
 			</ul>
 		</div>
